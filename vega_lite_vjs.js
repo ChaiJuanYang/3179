e0e288278612e312f02640e65135801cfe80a77d
Chai Juan Yang <101877234+ChaiJuanYang@users.noreply.github.com>
2024-09-22 12:53:24 +0800

    Update vega_lite_vjs.js

--- vega_lite_vjs.js
+++ vega_lite_vjs.js
@@ -1,4 +1,4 @@
-var vg1 = "map_plot_3.json";
+var vg1 = "map_plot.json";
 vegaEmbed("#map_plot", vg1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
